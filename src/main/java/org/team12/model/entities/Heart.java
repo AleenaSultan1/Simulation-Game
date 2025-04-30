60d78e24fb90610b510f6b79c9110e37c72b4971
Radley Le <nxl001@bucknell.edu>
2025-04-30 15:20:55 -0400

    Added life system

---
+++ src/main/java/org/team12/model/entities/Heart.java
@@ -0,0 +1,22 @@
+/* *****************************************
+ * CSCI 205 - Software Engineering and Design
+ * Spring 2025
+ *
+ * Name: Radley Le
+ * Date: 4/30/25
+ * Time: 1:01 PM
+ *
+ * Project: csci205_final_project
+ * Package: org.team12.model.entities
+ * Class: Heart
+ *
+ * Description:
+ *
+ * ****************************************
+ */
+
+package org.team12.model.entities;
+
+public class Heart extends Item{
+
+}
